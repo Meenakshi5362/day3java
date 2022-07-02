f70dda47e45c34753c5c8bb7c9f39d688a0d3935
unknown <annapureddy738@gmail.com>
2022-07-02 13:07:36 +0530

    Employee present or not

---
+++ EmpUc1.java
@@ -0,0 +1,20 @@
+public class EmpUc1 {
+	
+	public static void main(String[] args){
+		final int PRESENT = 1;
+		int ispresent = (int)(Math.random() *2);
+		System.out.println(ispresent);
+		
+		if(ispresent == PRESENT) {
+			System.out.println("EMPLOYEE IS PRESENT");
+		}
+			 else {
+			System.out.println("EMPLOYEE IS ABSENT");
+			 }
+		     
+		
+		
+	}
+	
+	
+}
