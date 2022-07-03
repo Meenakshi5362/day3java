5e040b52326f1607c79a9ee47136eb1cce3602fe
Meenakshi5362 <107310802+Meenakshi5362@users.noreply.github.com>
2022-07-03 10:22:57 +0530

    Update EmpUc5.java

--- EmpUc5.java
+++ EmpUc5.java
@@ -1,29 +1,45 @@
+
 public class EmpUc5 {
 	public static void main(String[] args) {
 		final int FULLTIME = 1;
 		final int PARTTIME = 2;
-		int workingdayspermonth=20;
-		
-		int hour;
-		int ispresent = (int) (Math.random() * 3);
-		System.out.println(ispresent);
+		final int wageperhr = 20;
 
-		
+		int emphour = 0;
+		int empwage = 0;
+		int totalempwage = 0;
+		System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
 
-		
-		switch (ispresent) {
-		case FULLTIME:
-			hour= 8;
-			break;
-		case PARTTIME:
-			hour=4;
-		    break;
-		default:
-			hour=0;
-			
+		final int workingdayspermonth = 20;
+
+		for (int day = 0; day < workingdayspermonth; day++) {
+
+			int isEmpPresent = (int) (Math.random() * 3);
+
+			switch (isEmpPresent) {
+
+			case FULLTIME:
+				System.out.println("Employee is present");
+				emphour = 8;
+				break;
+
+			case PARTTIME:
+				System.out.println("Employee is working part-time");
+				emphour = 4;
+				break;
+
+			default:
+				System.out.println("Employee is absent");
+				emphour = 0;
+				break;
+
+			}
+			empwage = emphour * wageperhr;
+			totalempwage = totalempwage + empwage;
+			System.out.println("Employee wage is" + empwage);
+
 		}
+		System.out.println("Employee monthlywage is" + totalempwage);
 
-		int wage = hour*workingdayspermonth; 
-		System.out.println("Employee wage per month is"+wage);
 	}
 }
